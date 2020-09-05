c0e00b08df7248f62abf0110d2cb5bf41809f64e
user <cafilho99@gmail.com>
2020-09-05 08:44:30 -0400

    Correcao do erro da ultima aula

--- src/aula1_node.cpp
+++ src/aula1_node.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h" // Incluir o core do ROS
 #include "geometry_msgs/Twist.h" // Permite o uso do Twist
+#include <stdlib.h>
 
 int main(int ac, char * av[]) {
 	ros::init(ac, av, "aula1_node"); // Falar pro ROS que esse vai ser um no
@@ -7,17 +8,17 @@ int main(int ac, char * av[]) {
 
 	ros::Publisher cmd_pub = n.advertise< geometry_msgs::Twist  >("/cmd_vel_mux/input/teleop", 10 );
 	
-		
+	srand(time(NULL));
 	ros::Rate loop_rate(10);
 	while( ros::ok() ) {
 
 		// Criar mensagem e popular
 		geometry_msgs::Twist cmd_msg;
-		cmd_msgs.linear.x = 0.5;
-		cmd_msgs.angular.z = 0.2;
+		cmd_msg.linear.x = 0.5;
+		cmd_msg.angular.z = 0.5 * ( 2 * (rand() % 5) / 5.0 - 1 ) ;
 		
 		// Mandar mensagem
-		cmd_pub.publish(cmd_msgs);
+		cmd_pub.publish(cmd_msg);
 
 		loop_rate.sleep();
 	}
